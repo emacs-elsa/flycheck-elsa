444d94254989732f4f221e08f2072c087af61922
jcs090218 <jcs090218@gmail.com>
2023-02-16 02:16:48 -0800

    feat: Run with Eask

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 [![MELPA](https://melpa.org/packages/flycheck-elsa-badge.svg)](https://melpa.org/#/flycheck-elsa)
+[![CI](https://github.com/emacs-elsa/flycheck-elsa/actions/workflows/test.yml/badge.svg)](https://github.com/emacs-elsa/flycheck-elsa/actions/workflows/test.yml)
 
 # flycheck-elsa - Flycheck for Elsa
 
 Integration of [Elsa](https://github.com/emacs-elsa/Elsa) into [Flycheck](https://github.com/flycheck/flycheck).
-
-# Elsa in Cask projects
-
-The recommended way to use Elsa is with [Cask](https://github.com/cask/cask).
 
 ## Installation
 
@@ -17,6 +14,27 @@ Stable](http://stable.melpa.org/) and add the following to your
 ``` emacs-lisp
 (add-hook 'emacs-lisp-mode-hook #'flycheck-elsa-setup)
 ```
+
+## How do I use this?
+
+The recommended way to use Elsa is with [Eask][Eask] or [Cask][Cask].
+
+```emacs-lisp
+(setq flycheck-elsa-backend 'cask)  ; or 'eask
+```
+
+### Eask
+
+This method uses [Eask][Eask] and installs Elsa from [MELPA][MELPA].
+
+1. Make sure you have [Eask][Eask] installed and presented in your PATH environment.
+2. You need an Eask-file in your project, you can create it via `eask init` command
+3. Add `(depends-on "elsa")` to your Eask-file
+4. Run `eask install-deps`
+
+You are ready to go! Open an elisp file and `M-x flycheck-mode`!
+
+### Cask
 
 We require that `cask` executable is usable from Emacs.  You can test
 this by evaluating `(executable-find "cask")`.  If this returns `nil`,
@@ -34,3 +52,7 @@ to initialize your `exec-path` from your shell's `$PATH`.
 ## Usage
 
 Just use Flycheck as usual in your [Cask](https://github.com/cask/cask) projects.
+
+[Cask]: https://github.com/cask/cask
+[Eask]: https://github.com/emacs-eask/cli
+[MELPA]: https://melpa.org
--- flycheck-elsa.el
+++ flycheck-elsa.el
@@ -1,4 +1,4 @@
-;;; flycheck-elsa.el --- Flycheck for Elsa. -*- lexical-binding: t -*-
+;;; flycheck-elsa.el --- Flycheck for Elsa  -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2018 Matúš Goljer
 
@@ -6,7 +6,7 @@
 ;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
 ;; Version: 1.0.0
 ;; Created: 23rd August 2018
-;; Package-requires: ((emacs "25") (seq "2.0"))
+;; Package-requires: ((emacs "25") (flycheck "0.14") (seq "2.0"))
 ;; Keywords: convenience
 ;; Homepage: https://github.com/emacs-elsa/flycheck-elsa
 
@@ -30,6 +30,7 @@
 ;;; Code:
 
 (require 'flycheck)
+(require 'cl-lib)
 (require 'seq)
 
 (defgroup flycheck-elsa nil
@@ -38,36 +39,62 @@
   :group 'flycheck
   :link '(url-link :tag "Github" "https://github.com/emacs-elsa/flycheck-elsa"))
 
+(defcustom flycheck-elsa-backend 'cask
+  "Choose what backend to use to power it up.
+The value can only be the symbol `cask' or `eask'.
+
+If value is `cask', then Cask will be used as the backend to check for the
+current file.  This means you must have a Cask-file presented in your project.
+
+If value is `eask', the conditions are very similar to Cask. But instead, you
+will need an Eask-file and not Cask-file."
+  :group 'flycheck-elsa
+  :type '(choice
+          (const :tag "Cask" 'cask)
+          (const :tag "Eask" 'eask)))
+
 (defcustom flycheck-elsa-ignored-files-regexps '(
                                                  "\\`Cask\\'"
+                                                 "\\`Eask\\'"
                                                  )
   "List of regular expressions matching files which should be ignored by Elsa."
   :group 'flycheck-elsa
   :type '(repeat regexp))
 
-(defun flycheck-elsa--locate-cask-dir ()
-  "Return dir located Cask file.  If missing, return nil."
-  (when-let ((filename (buffer-file-name))
-             (file (locate-dominating-file filename "Cask")))
+(defun flycheck-elsa--config-file ()
+  "Return the target configuration filename by variable `flycheck-elsa-backend'."
+  (cl-case flycheck-elsa-backend
+    (`cask "Cask")
+    (`eask "Eask")
+    (t (user-error "Unknown Elsa backend: %s" flycheck-elsa-backend))))
+
+(defun flycheck-elsa--locate-config-dir ()
+  "Return dir located config file.  If missing, return nil."
+  (when-let* ((filename (buffer-file-name))
+              (config (flycheck-elsa--config-file))
+              (file (locate-dominating-file filename config)))
     (file-name-directory file)))
 
 (defun flycheck-elsa--elsa-dependency-p ()
-  "Return non-nil if elsa listed in Cask file dependency."
-  (let ((cask-dir (flycheck-elsa--locate-cask-dir)))
+  "Return non-nil if elsa listed in config file dependency."
+  (let ((config-dir (flycheck-elsa--locate-config-dir))
+        (config (flycheck-elsa--config-file)))
     (with-temp-buffer
-      (insert-file-contents (expand-file-name "Cask" cask-dir))
+      (insert-file-contents (expand-file-name config config-dir))
       (let* ((contents (read (format "(%s)" (buffer-string))))
              (deps (append
                     (mapcan
                      (lambda (elm)
-                       (and (eq 'depends-on (car elm)) (list elm))) contents)
+                       (and (eq 'depends-on (car elm)) (list elm)))
+                     contents)
                     (mapcan
                      (lambda (elm)
                        (and (eq 'development (car elm))
                             (mapcan
                              (lambda (elm)
                                (and (eq 'depends-on (car elm)) (list elm)))
-                             (cdr elm)))) contents))))
+                             (cdr elm))))
+                     contents))))
         (and (delq 'nil (mapcar
                          (lambda (elm) (string= "elsa" (nth 1 elm))) deps))
              t)))))                     ; normarize return value
@@ -75,10 +102,10 @@
 (defun flycheck-elsa--enable-p ()
   "Return non-nil if we can enable Elsa in current buffer.
 
-We require that the project is managed by Cask and that Elsa is
+We require that the project is managed by config file and that Elsa is
 listed as a dependency."
-  (when-let (cask-dir (flycheck-elsa--locate-cask-dir))
-    (let ((default-directory cask-dir))
+  (when-let (config-dir (flycheck-elsa--locate-config-dir))
+    (let ((default-directory config-dir))
       (and (buffer-file-name)
            (not (seq-find (lambda (f) (string-match-p f (buffer-file-name)))
                           flycheck-elsa-ignored-files-regexps))
@@ -87,12 +114,24 @@ listed as a dependency."
 (defun flycheck-elsa--working-directory (&rest _)
   "Return the working directory where the checker should run."
   (if (buffer-file-name)
-      (flycheck-elsa--locate-cask-dir)
+      (flycheck-elsa--locate-config-dir)
     default-directory))
 
+(defun flycheck-elsa-command ()
+  "Return a list of strings for the command to execute and its arguments.
+The result depends on the variable `flycheck-elsa-backend'."
+  (cl-case flycheck-elsa-backend
+    (`cask '("cask" "exec" "elsa"))
+    (`eask '("eask" "exec" "elsa"))
+    (t (user-error "Unknown Elsa backend: %s" flycheck-elsa-backend))))
+
 (flycheck-define-checker emacs-lisp-elsa
-  "An Emacs Lisp checker using Elsa"
-  :command ("cask" "exec" "elsa" source-inplace)
+  "An Emacs Lisp checker using Elsa."
+  :command (;; flycheck forces us to pass a string as first argument:
+            ;; https://github.com/flycheck/flycheck/issues/1515
+            "emacs"
+            (eval (cdr (flycheck-elsa-command)))
+            source-inplace)
   :working-directory flycheck-elsa--working-directory
   :predicate flycheck-elsa--enable-p
   :error-filter flycheck-increment-error-columns
@@ -102,11 +141,17 @@ listed as a dependency."
    (info line-start (file-name) ":" line ":" column ":notice:" (message (one-or-more not-newline) (* "\n" (one-or-more " ") (one-or-more not-newline)))))
   :modes (emacs-lisp-mode))
 
+(defun flycheck-elsa--setup-executable ()
+  "Configure `flycheck-emacs-lisp-elsa-executable'."
+  (setq-local flycheck-emacs-lisp-elsa-executable
+              (car (flycheck-elsa-command))))
+
 ;;;###autoload
 (defun flycheck-elsa-setup ()
   "Setup Flycheck with Elsa."
   (interactive)
-  (add-to-list 'flycheck-checkers 'emacs-lisp-elsa))
+  (add-to-list 'flycheck-checkers 'emacs-lisp-elsa)
+  (add-hook 'flycheck-before-syntax-check-hook #'flycheck-elsa--setup-executable))
 
 (provide 'flycheck-elsa)
 ;;; flycheck-elsa.el ends here
